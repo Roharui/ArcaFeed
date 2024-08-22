488f308b3996d3b34884835f62158af8793bd6a1
Roharui <jack555586@gmail.com>
2024-08-22 14:29:40 +0900

    fix prev page

--- src/event/next.js
+++ src/event/next.js
@@ -6,7 +6,7 @@ function nextPage() {
     }
     var x = document.querySelector("a.active + a")
     var xx = document.querySelector("li.active + li > a")
-    var xxx = document.querySelector("a.vrow.column")
+    var xxx = document.querySelector("a.vrow.column:not(.notice)")
     var endOfPage = (location.pathname.match(/\//g) || []).length === 2
     if(x == null && endOfPage) {
         location.href = xxx.href
@@ -21,7 +21,17 @@ function prevPage() {
     if (new Vault().viewer) {
         return
     }
-    history.back()
+    var x = $("a.vrow.column.active").prev("a:not(.notice)")
+    var xx = $("li.active").prev("li").find("a")
+    var xxx = $("a.vrow.column:not(.notice):last")
+    var endOfPage = (location.pathname.match(/\//g) || []).length === 2
+    if(!x.length && endOfPage) {
+        location.href = xxx.attr("href")
+    } else if (!x.length && !endOfPage) {
+        location.href = xx.attr("href")
+    } else {
+        location.href = x.attr("href")
+    }
 }
 
 export { prevPage, nextPage }
