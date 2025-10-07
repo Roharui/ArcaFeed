593ac221452da7f9aa0e563e14efdbe9c1bbeb7b
Roharui <jack555586@gmail.com>
2025-10-08 08:06:15 +0900

    disable log at production

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import '@swiper/swiper.css';
 import '@css/arcalive.css';
 
 (function() {
+  if (process.env.NODE_ENV === 'production') {
+    console.log = () => { }
+  }
+
   const helper = new Helper();
   helper.init();
 })();
